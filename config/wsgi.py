1617c211bcfedae9d3d8fd210189168370f10b98
Daniel Muñoz <juandanielmunozurquijo@gmail.com>
2026-03-26 22:05:16 -0500

    feat: forzar migraciones en wsgi.py (opción nuclear para Railway)

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# Nuclear option: Run migrations and create_admin at startup
+try:
+    print("--- Bootstrapping Database ---")
+    call_command('migrate', interactive=False)
+    call_command('create_admin')
+    print("--- Bootstrap Complete ---")
+except Exception as e:
+    print(f"--- Bootstrap Error: {e} ---")
 
 application = get_wsgi_application()
